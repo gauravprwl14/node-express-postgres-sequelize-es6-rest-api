40a7b6845a474c78e3156edffef55ca2fb5e0cc8
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-15 20:34:29 +0530

    fix linting errors in the gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,20 +13,19 @@ const paths = {
 };
 
 // Clean up dist and coverage directory
-gulp.task('clean', () =>
-    del.sync(['dist/**', 'dist/.*', 'coverage/**', '!dist', '!coverage'])
-);
+gulp.task('clean', () => del.sync(['dist/**', 'dist/.*', 'coverage/**', '!dist', '!coverage']));
 
 // Copy non-js files to dist
 gulp.task('copy', () =>
-    gulp.src(paths.nonJs)
+    gulp
+        .src(paths.nonJs)
         .pipe(plugins.newer('dist'))
-        .pipe(gulp.dest('dist'))
-);
+        .pipe(gulp.dest('dist')));
 
 // Compile ES6 to ES5 and copy to dist
 gulp.task('babel', () =>
-    gulp.src([...paths.js, '!gulpfile.babel.js'], { base: '.' })
+    gulp
+        .src([...paths.js, '!gulpfile.babel.js'], { base: '.' })
         .pipe(plugins.newer('dist'))
         .pipe(plugins.sourcemaps.init())
         .pipe(plugins.babel())
@@ -36,8 +35,7 @@ gulp.task('babel', () =>
                 return path.relative(file.path, __dirname);
             },
         }))
-        .pipe(gulp.dest('dist'))
-);
+        .pipe(gulp.dest('dist')));
 
 // Start server with restart on file changes
 gulp.task('nodemon', ['copy', 'babel'], () =>
@@ -46,15 +44,12 @@ gulp.task('nodemon', ['copy', 'babel'], () =>
         ext: 'js',
         ignore: ['node_modules/**/*.js', 'dist/**/*.js'],
         tasks: ['copy', 'babel'],
-    })
-);
+    }));
 
 // gulp serve for development
 gulp.task('serve', ['clean'], () => runSequence('nodemon'));
 
 // default task: clean dist, compile js files and copy non-js files.
 gulp.task('default', ['clean'], () => {
-    runSequence(
-        ['copy', 'babel']
-    );
+    runSequence(['copy', 'babel']);
 });
