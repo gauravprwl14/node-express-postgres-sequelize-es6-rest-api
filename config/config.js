cbd8890256494aa0262fb7deea3cbcdc40a852bd
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-15 20:13:54 +0530

    getting env variable from the .env file and adding configuration properties related to the project and DB

---
+++ config/config.js
@@ -0,0 +1,48 @@
+import Joi from 'joi';
+
+// require and configure dotenv, will load vars in .env in PROCESS.ENV
+require('dotenv').config();
+
+// define validation for all the env vars
+const envVarsSchema = Joi.object({
+    NODE_ENV: Joi.string()
+        .allow(['development', 'production', 'test', 'provision'])
+        .default('development'),
+    PORT: Joi.number().default(4000),
+    JWT_SECRET: Joi.string()
+        .required()
+        .description('JWT Secret required to sign'),
+    PG_DB: Joi.string()
+        .required()
+        .description('Postgres database name'),
+    PG_PORT: Joi.number().default(5432),
+    PG_HOST: Joi.string().default('localhost'),
+    PG_USER: Joi.string()
+        .required()
+        .description('Postgres username'),
+    PG_PASSWORD: Joi.string()
+        .allow('')
+        .description('Postgres password'),
+})
+    .unknown()
+    .required();
+
+const { error, value: envVars } = Joi.validate(process.env, envVarsSchema);
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+const config = {
+    env: envVars.NODE_ENV,
+    port: envVars.PORT,
+    jwtSecret: envVars.JWT_SECRET,
+    postgres: {
+        db: envVars.PG_DB,
+        port: envVars.PG_PORT,
+        host: envVars.PG_HOST,
+        user: envVars.PG_USER,
+        password: envVars.PG_PASSWORD,
+    },
+};
+
+export default config;
