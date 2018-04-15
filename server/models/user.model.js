f9d2dd75c0d9b31672cbc721d7e2e59ab1111872
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-15 20:43:46 +0530

    added user model

---
+++ server/models/user.model.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * User Schema
+ */
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    });
+
+    return User;
+};
