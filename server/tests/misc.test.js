abeb992fa8605243e027a14ba383549e1df5fa96
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-15 20:56:24 +0530

    fix linting error in auth.test and misc.test js

---
+++ server/tests/misc.test.js
@@ -0,0 +1,52 @@
+/* eslint-env mocha */
+
+import request from 'supertest';
+import httpStatus from 'http-status';
+import chai, { expect } from 'chai';
+import app from '../../index';
+
+chai.config.includeStack = true;
+
+describe('## Misc', () => {
+    describe('# GET /api/health-check', () => {
+        it('should return OK', (done) => {
+            request(app)
+                .get('/api/health-check')
+                .expect(httpStatus.OK)
+                .then((res) => {
+                    expect(res.text).to.equal('OK');
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe('# GET /api/404', () => {
+        it('should return 404 status', (done) => {
+            request(app)
+                .get('/api/404')
+                .expect(httpStatus.NOT_FOUND)
+                .then((res) => {
+                    expect(res.body.message).to.equal('Not Found');
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe('# Error Handling', () => {
+        it('should handle express validation error - username is required', (done) => {
+            request(app)
+                .post('/api/users')
+                .send({
+                    mobileNumber: '1234567890',
+                })
+                .expect(httpStatus.BAD_REQUEST)
+                .then((res) => {
+                    expect(res.body.message).to.equal('"username" is required');
+                    done();
+                })
+                .catch(done);
+        });
+    });
+});
